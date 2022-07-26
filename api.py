37d148a7b1fcfc7c1976f97dd77295e876700553
mrunmayee17 <mrunmayeerane17@gmail.com>
2022-07-26 12:14:26 -0400

    changed directory paths

--- api.py
+++ api.py
@@ -2,21 +2,22 @@ import uvicorn
 from fastapi import FastAPI
 import joblib
 
+
+# Loading sentiment classifer model and text to vector model
 classifier = joblib.load(open("./model/sentiment_classifer.pkl","rb"))
 vectorizer = joblib.load(open("./model/countvectorizer.pkl","rb"))
 
 app = FastAPI()
-
+#routes
 @app.get('/')
 async def index():
-  return {"text":'Sentiment Analysis on Text'}
+  return {"text":'Predict the sentiment of the text!'}
 
 
 @app.post('/classify')
 def classify(data:str):
-    # vectorizer = CountVectorizer(ngram_range=(1, 2), stop_words='english', min_df=20)
     result = classifier.predict_proba(vectorizer.transform([data]).toarray())[0][1]
-    print(result)
+    # print(result)
     if result > 0.5:
         return {"Sentiment of text": 'Positive'}
     else:
